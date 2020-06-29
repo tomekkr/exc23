25af8740226755816ea3bebcecf8178369de5475
Tomek Kruszyna <tomek.kruszyna@gmail.com>
2020-06-29 20:01:43 +0200

    utworzyłem projekt

---
+++ src/main/java/pl/tk/exc23/Exc23Application.java
@@ -0,0 +1,13 @@
+package pl.tk.exc23;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exc23Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Exc23Application.class, args);
+    }
+
+}
